abd97a0bdee117fd7aba23e97ce2cd40783167f0
MilanVadhel01 <milanvadhel01@gmail.com>
2026-01-03 15:02:36 +0530

    fix: await MongoDB connection in Vercel serverless handler

--- api/index.js
+++ api/index.js
@@ -1,6 +1,9 @@
+const connectDB = require('../backend/config/db');
 const app = require('../backend/server');
 
 // Vercel Serverless Function that wraps the Express app
-module.exports = (req, res) => {
+module.exports = async (req, res) => {
+    // Ensure MongoDB is connected before handling request
+    await connectDB();
     app(req, res);
 };
